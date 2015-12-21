8bfd71df22f2bfd5ea30ab6f9ea60cb97883fae4
Aaron William West <tallpeak@hotmail.com>
2015-12-21 12:37:44 -0800

    Update euler92b.fsx
    Now 350 ms compiled, Xeon W3550

--- euler92b.fsx
+++ euler92b.fsx
@@ -1,5 +1,6 @@
-// orig from http://theburningmonk.com/2010/09/project-euler-problem-92-solution/
 // orig 17 seconds, now 1.1 seconds
+//from
+//http://theburningmonk.com/2010/09/project-euler-problem-92-solution/
 open System
 open System.Diagnostics 
 let t0 = new Stopwatch()
@@ -14,8 +15,9 @@ let addSquaredDigits (n:int) : int =
           if x = 0 then s else go (s + square(x % 10)) (x / 10)
      in go 0 n
 
-// build up a cache for just the first 568 or so numbers
-let cache = Array.init (addSquaredDigits (max-1) + 1) (fun n ->
+// build up a cache for just the first 568 or so numbers; (9^2)*7 = 568
+let cacheSize = addSquaredDigits (max-1) + 1  
+let cache = Array.init cacheSize (fun n ->
     match n with
     | 0 | 1 -> Some(false)
     | 89 -> Some(true)
@@ -25,18 +27,20 @@ let cache = Array.init (addSquaredDigits (max-1) + 1) (fun n ->
 // it gets to a number whose subsequent chain ends with 1 or 89, which means that
 // all previous numbers will also end in the same number
 let processChain n =
-    let rec processChainRec n (list: int list) : bool option=
+    let rec processChainRec n (list: int list) =
         if cache.[n] = None then processChainRec (addSquaredDigits n) (list@[n])
         else 
                 list |> List.iter (fun n' -> cache.[n'] <- cache.[n])
-                cache.[n]
-    processChainRec (addSquaredDigits n) []
- 
+    processChainRec n []
+
+for i = 2 to cacheSize - 1 do 
+    processChain i
+
 // go through all the numbers from 2 to 10 million using the above function
 let mutable answer = 0
 for i = 2 to max do
     answer <- answer + 
-        match (processChain i) 
+        match (cache.[addSquaredDigits i]) 
          with | Some(true) -> 1
                 | _ -> 0
  
