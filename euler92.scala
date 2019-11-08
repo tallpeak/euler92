79fb47d4bd6ad92fd3fc905c2bdf4e325ea7099d
Aaron William West <tallpeak@hotmail.com>
2019-11-08 10:12:45 -0800

    Update euler92.scala

--- euler92.scala
+++ euler92.scala
@@ -1,44 +1,11 @@
 import java.util.Date
 // 1.6 seconds with no memoization
-// 7.0 seconds generic-memoized
+// 7.0 seconds generic-memoized (removed this code)
 // 0.9 seconds with Array[Int] size 10000
 // 1.1 seconds with Array[Int] size 1000
 // 0.75 seconds with Array[Int] size 1000 but ifs to only read the array when needed
 
-object euler92 {
-	
-		case class memo(f: Int => Int) {
-		  // We aren't going to synchronize cache access, because
-		  // it's harmless if two threads right the same key-value
-		  // to the cache simultaneously!
-		  val cache = scala.collection.mutable.Map.empty[Int,Int]
-		  def apply(i: Int): Int = cache.get(i) match {
-		    case Some(l) => l
-		    case None =>
-		      val l = f(i)
-		      cache.put(i,l)
-		      l
-		  }
-		}
-		
-			// Implementation without using pattern matching.
-		case class memo2(f: Int => Int) {
-		  val cache = scala.collection.mutable.Map.empty[Int,Int]
-		  def apply(i: Int): Int = {
-		    val optionl = cache.get(i)
-		    if (optionl == None) {
-		      val l = f(i)
-		      cache.put(i,l)
-		      l
-		    } else {
-		      optionl.get
-		    }
-		  }
-		};
-		//import org.scalaide.worksheet.runtime.library.WorksheetSupport._;
-		
-		//def main(args: Array[String])
-			
+object euler92 {		
 
      def digits(n: Int): List[Int] = {
          var x = n
@@ -65,6 +32,7 @@ object euler92 {
  
      val ssdCache: Array[Int] = (0 to cacheSize-1).map(i => sumSquareDigits(i)).toArray
 
+     //equivalent, more imperitive approach to the above line:
      //var ssdCache: Array[Int] = Array(0, cacheSize)
      //Array.fill(cacheSize)(0)
      //for (i <- 0 to (cacheSize-1))
@@ -76,8 +44,8 @@ object euler92 {
      def ssd2(n: Int) : Int =	ssdCache(n % 1000) +
      				(if (n > 999) 	ssdCache(n / 1000 % 1000) 	 else 0) +
 	     			(if (n > 999999)ssdCache(n / 1000000 % 1000) else 0)
-
-		 def sumSquareDigits_memo = memo(sumSquareDigits)
+     
+     def sumSquareDigits_memo = memo(sumSquareDigits)
 
      def terminator(x: Int): Int = {
          if (x == 89 || x == 1) x
@@ -95,31 +63,8 @@ object euler92 {
          val t1 = System.nanoTime()
          val elapsed = t1 - t0
          (c, elapsed / 1e9)
-     };//System.out.println("""count89: (n: Int)(Int, Double)""");$skip(28);
-
+     };
+	
      val tenmil = 10000000
-     count89(tenmil)
-     
+     count89(tenmil)    
 }
-
-
-
-//class Memoize1[-T, +R](f: T => R) extends (T => R) {
-//  import scala.collection.mutable
-//  private[this] val vals = mutable.Map.empty[T, R]
-//
-//  def apply(x: T): R = {
-    //if (vals.contains(x)) {
-//      vals(x)
-    //}
-    //else {
-    //  val y = f(x)
-//      vals += ((x, y))
-      //y
-//    }
-//  }
-//}
- 
-//object Memoize1 {
-//  def apply[T, R](f: T => R) = new Memoize1(f)
-//}
