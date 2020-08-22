0867ecd88743c869e785ab8c3d6d03bc7d407064
Aaron West <tallpeak@gmail.com>
2020-08-22 07:17:57 -0700

    unsuccessful caching; proving that julia uses multiply by inverse instead of division by 10

--- euler92.jl
+++ euler92.jl
@@ -13,24 +13,43 @@ function sumSquareDigits(n)
     return s
 end
 
-function nc(n) 
-    if n > 1 && n != 89 && n != 4 
-        return nc(sumSquareDigits(n))
+# This cache is not helpful:
+
+# ssdCache = Array{Int16}(undef,1000)
+# for i in 0:999
+#     ssdCache[i+1] = sumSquareDigits(i)
+# end
+
+# function ssd2(n)
+#     return  ssdCache[div(n,1_000_000)%1000+1] + 
+#             ssdCache[div(n,1_000)%1000+1] + 
+#             ssdCache[n%1000+1] 
+# end
+
+function terminator(n) 
+    if n > 1 && n != 89 
+        return terminator(sumSquareDigits(n))
+        # surprisingly, this does not improve the timing! :
+        # return terminator(ssd2(n)) 
+        # Julia is optizing div by a constant as multiplication by the inverse!
+        # good job, compiler writers!!
     else
         return n > 1
     end 
 end
+
 function makecache()
-    global cache = BitArray([ nc(i) for i in 1:cachelen+1 ])
+    global cache = BitArray([ terminator(i) for i in 1:cachelen+1 ])
 end
+
 makecache()
+
 function answer(n::Int64)
-    local pe92 = Int64(0)
-    for i in 1:n
-        pe92 += cache[sumSquareDigits(i)]
-    end
-    return pe92
+    return sum(cache[sumSquareDigits(i)] for i=1:n)
 end
 
 print("Answer to PE92 = ",answer(N))
-@time(answer(N)) # 0.6 seconds on macbook pro 2015 i7 
+@time(answer(N)) # About 0.6 seconds on macbook pro 2015 i7 
+# @code_native(sumSquareDigits(1)) 
+# multiplication by inverse used:
+#        movabsq $7378697629483820647, %r8 # 2^66/10 ...
