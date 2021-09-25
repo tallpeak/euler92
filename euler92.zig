d842f9a3a60fab7a0ae4b967ebbdb1ae920bd805
Aaron West <tallpeak@gmail.com>
2021-09-24 21:03:21 -0700

    add caching. Saved 0.1 sec

--- euler92.zig
+++ euler92.zig
@@ -1,19 +1,35 @@
 const std = @import("std");
-
-
 const LIMIT : u32 = 10000000;
+//var sqdigit : [10]u32 = .{0,1,4,9,16,25,36,49,64,81};
+fn sq(x:u32) u32 {
+    return x*x;
+}
 
-var sqdigit : [10]u32 = .{0,1,4,9,16,25,36,49,64,81};
-
-fn ssd(x : u32) u32
+fn ssd(x : u32) u32 // sum of squares of digits
 {
     var s : u32 = 0;
     var t : u32 = x;
     while (t>0) {
-        s += sqdigit[t % 10];
+        s += sq(t%10); //sqdigit[t % 10];
         t /= 10;
     }
     return s;
+}
+
+// use compile-time code to initialize an array
+var ssdCache = init : {
+    var initial_value: [1000]u16 = undefined;
+    @setEvalBranchQuota(10001);
+    for (initial_value) |*p, i| {
+        p.* = ssd(i);
+    }
+    break :init initial_value;
+};
+
+fn ssd2(x : u32) u32 {
+    return ssdCache[x % 1000] + 
+        (if (x > 999) ssdCache[ x / 1000 % 1000] else 0) + 
+        (if (x > 999999) ssdCache[ x / 1000000 % 1000] else 0) ;
 }
 
 fn termination(x : u32) u32
@@ -21,7 +37,7 @@ fn termination(x : u32) u32
     var t = x;
     while (t != 1 and t != 89)
     {
-        t = ssd(t);
+        t = ssd2(t);
     }
     return t;
 }
@@ -32,13 +48,13 @@ fn countT89() u32
     var i : u32 = 1;
     while (i < LIMIT)
     {
-        //printf("%d=%d %d\t",i,termination(i),count);
         if (termination(i) == 89)
             count += 1;
         i += 1;
     }
     return count;
 }
+
 pub fn main() void {
     var cnt = countT89();
     std.debug.print("count terminating at 89={}\n",.{cnt});
